53040b25a5f900cb62d6cae55f302ecd33ec754e
mmerguoum <merguoummourad@gmail.com>
2021-07-05 17:01:45 +0100

    API

---
+++ API/scripts/script.js
@@ -0,0 +1,9 @@
+let cardTitle = "elzero",
+cardDescription = "Elzero web school",
+cardDate = 25/10;
+let card = `<div classe container>
+            <h3> ${cardTitle} </h3>
+            <p>${cardDescription}</p>
+            <span>${cardDate}</span>
+            </div>`;
+            document.write(pageStructure.repeat(4));
